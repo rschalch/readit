659fe0b60160af550477f4220b5b2429475f388b
Ricardo Bürger Schalch <ricardo.schalch@gmail.com>
2016-07-25 17:08:18 -0300

    Register Book in admin and improve Book model

--- books/models.py
+++ books/models.py
@@ -6,7 +6,12 @@ from django.db import models
 class Book(models.Model):
     # primary key is auto-generated
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=70)
-    review = models.TextField(blank=True, null=True) # field not required
+    author = models.CharField(max_length=70, help_text="Use pen name, not real name")
+    review = models.TextField(blank=True, null=True)  # field not required
     date_reviewed = models.DateTimeField(blank=True, null=True)
-    is_favourite = models.BooleanField(default=False)
+    # by default Django displays the field capitalizing the field and substituting underscores by spaces
+    # but we can choose how to display it by changing the verbose_name attribute
+    is_favourite = models.BooleanField(default=False, verbose_name="Favourite ?")
+
+    def __str__(self):
+        return self.title
