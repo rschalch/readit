e130ab3085591187cd0a14bc58e5ae360e75a5ea
Ricardo Bürger Schalch <ricardo.schalch@gmail.com>
2016-07-27 00:45:32 -0300

    Create custom BookAdmin

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
 from .models import Book, Author
 
-# Register your models here.
 
-admin.site.register(Book)
+# instead of using admin.site.register(Book, BookAdmin) we can use a register decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Book Details", {"fields": ["title", "authors"]}),
+        ("Review", {"fields": ["is_favourite", "review", "date_reviewed"]}),
+    ]
+
+    readonly_fields = ("date_reviewed",)
+
+    # since we cannot display authors as it's a manytomany field
+    # we must create a method that returns a list of authors from
+    # the given obj, in this particular case, a BOOK
+    def book_authors(self, obj):
+        return obj.list_authors()
+
+    book_authors.short_description = "Author(s)"
+
+    # "book_authors" refers to the above method
+    list_display = ("title", "book_authors", "date_reviewed", "is_favourite")
+    list_editable = ("is_favourite",)
+
+    # allow the ordering of columns
+    list_display_links = ("title", "date_reviewed")
+
+    # add filters to list page
+    list_filter = ("is_favourite",)
+
+    # in order to search by authors we must access the Author's "name" field
+    # to do this we can traverse from authors to name by using double underscores
+    search_fields = ("title", "authors__name")
+
+
+# Register your models here.
 admin.site.register(Author)
