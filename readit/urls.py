1f36206a9ed8a99f01d39678e22fa32d1090ab7f
rbs <ricardo.schalch@gmail.com>
2016-08-16 17:04:31 -0300

    Refactor templates, add links and static resources

--- readit/urls.py
+++ readit/urls.py
@@ -21,7 +21,7 @@ from books.views import AuthorList, AuthorDetail, list_books, BookDetail
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', list_books, name="books"),
-    url(r'^authors/$', AuthorList.as_view(), name="books"),
+    url(r'^authors/$', AuthorList.as_view(), name="authors"),
     url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name="book-detail"),
     url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name="author-detail")
 ]
